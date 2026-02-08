c56b816d56f7f9b86ba6e0f0cfc6c2138ad16b55
firuzhajdarov844-debug <firuzhajdarov844@gmail.com>
2026-02-08 19:04:25 +0500

    Create engine.js

---
+++ core/engine.js
@@ -0,0 +1,21 @@
+let SYSTEM = {
+  difficulty: null,
+  world: {},
+  player: {},
+  ai: {}
+};
+
+function setDifficulty(mode){
+  SYSTEM.difficulty = mode;
+  localStorage.setItem("V4_DIFFICULTY", mode);
+  window.location.href = "class.html";
+}
+
+function loadDifficulty(){
+  return fetch("data/difficulties.json").then(r=>r.json());
+}
+
+function initSystem(){
+  SYSTEM.difficulty = localStorage.getItem("V4_DIFFICULTY");
+  SYSTEM.class = localStorage.getItem("V4_CLASS");
+}
